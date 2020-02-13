1794f24dbbfbd054fb2dcd83004dc72e1f690875
Venkat <venkatdevara98@gmail.com>
2020-02-13 12:42:53 +0530

    updated oops concepts and solid principles

--- OopsConcepts/LocalDateImplementation.java
+++
@@ -1,9 +0,0 @@
-import java.io.*;
-import java.util.*;
-import java.time.LocalDate;
-public class LocalDateImplementation{
-    void main(){
-        LocalDate date1=LocalDate.of(2017,1,3);
-        
-    }
-}
